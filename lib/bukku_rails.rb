d2d6ab222def0709da566e8531b89e24811de602
Muzaffar <mus.ariff@gmail.com>
2025-12-03 22:49:24 +0800

    Initial get implementatio

---
+++ lib/bukku_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bukku_rails/version"
+
+module BukkuRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bukku_rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BukkuRails
+  VERSION = "0.1.0"
+end
